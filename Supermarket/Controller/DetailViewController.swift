3ce4d8736b066269b4860f83b1505a9ff178fa8a
Ilya Masalov <xmasalov@gmail.com>
2019-06-27 20:08:49 +0400

    Add detail viewcontroller

---
+++ Supermarket/Controller/DetailViewController.swift
@@ -0,0 +1,18 @@
+//
+//  DetailViewController.swift
+//  Supermarket
+//
+//  Created by Ilya Masalov on 27/06/2019.
+//  Copyright © 2019 Ilya Masalov. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+    
+    // MARK: - Lifecycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
