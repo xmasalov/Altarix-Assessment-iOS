e3d1d30d53f77d52b0ea58460cefdedc641526cc
Ilya Masalov <xmasalov@gmail.com>
2019-06-27 17:06:39 +0400

    Add project

---
+++ Supermarket/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  Supermarket
+//
+//  Created by Ilya Masalov on 27/06/2019.
+//  Copyright © 2019 Ilya Masalov. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Supermarket/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Supermarket
+//
+//  Created by Ilya Masalov on 27/06/2019.
+//  Copyright © 2019 Ilya Masalov. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
