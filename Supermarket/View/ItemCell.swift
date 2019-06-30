f37508991dc2381fbd5b1d01c136b5ea933da8fc
Ilya Masalov <xmasalov@gmail.com>
2019-06-30 16:50:11 +0400

    Add Item Cell

---
+++ Supermarket/View/ItemCell.swift
@@ -0,0 +1,23 @@
+//
+//  ItemCell.swift
+//  Supermarket
+//
+//  Created by Ilya Masalov on 27/06/2019.
+//  Copyright © 2019 Ilya Masalov. All rights reserved.
+//
+
+import UIKit
+
+class ItemCell: UITableViewCell {
+    
+    // MARK: - Properties
+    @IBOutlet private weak var nameLabel: UILabel!
+}
+
+// MARK: - Methods
+extension ItemCell {
+    
+    func configure(name: String) {
+        nameLabel.text = name
+    }
+}
