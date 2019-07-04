aaada3e358c24d60dff428571e592ab4ad6b7dd9
Ilya Masalov <xmasalov@gmail.com>
2019-07-04 12:32:39 +0400

    Add DetailVIewModel

---
+++ Supermarket/ViewModel/DetailViewModel.swift
@@ -0,0 +1,99 @@
+//
+//  DetailViewModel.swift
+//  Supermarket
+//
+//  Created by Ilya Masalov on 01/07/2019.
+//  Copyright © 2019 Ilya Masalov. All rights reserved.
+//
+
+import Foundation
+import ReactiveKit
+import Bond
+
+class DetailViewModel {
+    
+    // MARK: - Properties
+    let editStateTitle = "Редактировать"
+    let doneStateTitle = "Готово"
+    let departmentAlertTitle = "Категория"
+    let departmentAlertMessage = "Выберите подходящую категорию"
+    let departmentAlertActionTitle = "Продолжить"
+    
+    private(set) var item: Item?
+    private(set) var itemManager: ItemManager
+    
+    private(set) lazy var itemName = Observable<String?>(item?.name ?? "")
+    private(set) lazy var itemDepartment = Observable<Item.Department>(item?.department ?? .none)
+    private(set) lazy var itemPrice = Observable<String?>(item?.price.description ?? "")
+    
+    private(set) var allowedToSaveEdits = Observable<Bool>(false)
+    
+    private let category: Item.Category
+    
+    init(item: Item?, itemManager: ItemManager, category: Item.Category) {
+        self.item = item
+        self.itemManager = itemManager
+        self.category = category
+        
+        addBindings()
+    }
+    
+    func addBindings() {
+        combineLatest(itemName, itemPrice) { name, price in
+            let nameFilled = !(name?.trimmingCharacters(in: .whitespaces).isEmpty ?? true)
+            let priceFilled = !(price?.trimmingCharacters(in: .whitespaces).isEmpty ?? true)
+            
+            return nameFilled && priceFilled
+            }.bind(to: allowedToSaveEdits)
+    }
+    
+    func writeChanges() {
+        if let item = item {
+            itemManager.update(item: item, name: itemName.value!, price: Double(itemPrice.value!)!, department: itemDepartment.value, category: category)
+        } else {
+            let item = Item()
+            item.name = itemName.value!
+            item.price = Double(itemPrice.value!)!
+            item.department = itemDepartment.value
+            item.category = category
+            itemManager.add(item: item)
+        }
+    }
+}
+
+// MARK: - Methods
+extension DetailViewModel {
+    
+    func shouldChangeCharactersIn(text oldText: String, range: NSRange, replacementString string: String) -> Bool {
+        guard let r = Range(range, in: oldText) else {
+            return true
+        }
+        
+        let newText = oldText.replacingCharacters(in: r, with: string)
+        let isNumeric = newText.isEmpty || (Double(newText) != nil)
+        let numberOfDots = newText.components(separatedBy: ".").count - 1
+        
+        let numberOfDecimalDigits: Int
+        if let dotIndex = newText.firstIndex(of: ".") {
+            numberOfDecimalDigits = newText.distance(from: dotIndex, to: newText.endIndex) - 1
+        } else {
+            numberOfDecimalDigits = 0
+        }
+        
+        return isNumeric && numberOfDots <= 1 && numberOfDecimalDigits <= 2
+    }
+    
+    func priceFormatter(value: Any?) -> String? {
+        guard let value = value else {
+            return nil
+        }
+        
+        let numberFormatter = NumberFormatter()
+        
+        numberFormatter.decimalSeparator = "."
+        numberFormatter.numberStyle = .decimal
+        numberFormatter.maximumFractionDigits = 2
+        
+        return numberFormatter.string(from: value as! NSNumber)
+    }
+}
