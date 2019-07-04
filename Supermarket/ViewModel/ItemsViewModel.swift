62ca9de386b1836bf9243cfecd47391b0338e1c5
Ilya Masalov <xmasalov@gmail.com>
2019-07-04 12:34:42 +0400

    Update ItemsViewModel

--- Supermarket/ViewModel/ItemsViewModel.swift
+++ Supermarket/ViewModel/ItemsViewModel.swift
@@ -6,37 +6,72 @@
 //  Copyright © 2019 Ilya Masalov. All rights reserved.
 //
 
+import ReactiveKit
 import Bond
 
 class ItemsViewModel {
     
-    // MARK: - Properties
-    private let itemManager: ItemManager
+    // MARK: - Enums
+    enum Filter: String, CaseIterable {
+        case all = "Все категории"
+        case home = "Для дома"
+        case appliances = "Бытовая техника"
+        case food = "Еда"
+        case clothes = "Одежда"
+        case misc = "Прочее"
+        
+        var title: String {
+            return rawValue
+        }
+    }
 
+    // MARK: - Properties
+    let departmentAlertTitle = "Категория"
+    let departmentAlertMessage = "Выберите подходящую категорию"
+    let departmentAlertActionTitle = "Выбрать"
+    
+    let itemManager: ItemManager
+    
     let itemsToBuy = MutableObservableArray<Item>()
     let itemsToSell = MutableObservableArray<Item>()
+    let bag = DisposeBag()
+    
+    var selectedFilter: Filter = .all {
+        didSet {
+            disposeModel()
+            bindModel()
+        }
+    }
     
     // MARK: - Init
     init(itemManager: ItemManager) {
         self.itemManager = itemManager
         bindModel()
-        itemManager.fetchData()
     }
 }
 
 // MARK: - Methods
 extension ItemsViewModel {
-
+    
     func addItem(item: Item) {
         itemManager.add(item: item)
     }
+    
+    // TODO: Implement remove
 }
 
 // MARK: - Private methods
-private extension ItemsViewModel {
+extension ItemsViewModel {
     
     func bindModel() {
-        itemManager.items.filterCollection({ $0.category == .buy }).bind(to: itemsToBuy)
-        itemManager.items.filterCollection({ $0.category == .sell }).bind(to: itemsToSell)
+        itemManager.items.filterCollection({ $0.category == .buy }).suppressError(logging: true).bind(to: itemsToBuy).dispose(in: bag)
+        
+        itemManager.items.filterCollection({ [unowned self] in
+            return self.selectedFilter == .all ? $0.category == .sell : $0.category == .sell && $0.department.title == self.selectedFilter.title
+        }).suppressError(logging: true).bind(to: itemsToSell).dispose(in: bag)
+    }
+    
+    func disposeModel() {
+        bag.dispose()
     }
 }
