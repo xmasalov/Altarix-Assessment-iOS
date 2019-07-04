b814d0ee196321c19c0c2263ca44ffaaca898feb
Ilya Masalov <xmasalov@gmail.com>
2019-07-04 12:35:29 +0400

    Fix navgiation bar tab bar background glitch

--- Supermarket/AppDelegate.swift
+++ Supermarket/AppDelegate.swift
@@ -24,6 +24,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         
+        window?.backgroundColor = .white
+        
         if isFirstTime {
             isFirstTime = false
             RealmItemManager.addSampleData()
