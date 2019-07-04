2b05f0915eb9f98c45597b03bec8baebb37def98
Ilya Masalov <xmasalov@gmail.com>
2019-07-04 12:36:35 +0400

    Implement viewmodel dependency injection

--- Supermarket/AppDelegate.swift
+++ Supermarket/AppDelegate.swift
@@ -10,7 +10,7 @@ import UIKit
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
+    
     var window: UIWindow?
     
     var isFirstTime: Bool {
@@ -31,31 +31,45 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
             RealmItemManager.addSampleData()
         }
         
+        let itemManager = RealmItemManager()
+        let viewModel = ItemsViewModel(itemManager: itemManager)
+        
+        if let tabBarViewController = window?.rootViewController as? UITabBarController,
+            let barViewControllers = tabBarViewController.viewControllers {
+            for viewController in barViewControllers {
+                if let navigationViewController = viewController as? UINavigationController,
+                    let viewController = navigationViewController.topViewController as? ItemsViewModelCompatible {
+                    
+                    viewController.viewModel = viewModel
+                }
+            }
+        }
+
         return true
     }
-
+    
     func applicationWillResignActive(_ application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
     }
-
+    
     func applicationDidEnterBackground(_ application: UIApplication) {
         // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
         // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
     }
-
+    
     func applicationWillEnterForeground(_ application: UIApplication) {
         // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
     }
-
+    
     func applicationDidBecomeActive(_ application: UIApplication) {
         // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
     }
-
+    
     func applicationWillTerminate(_ application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
-
-
+    
+    
 }
 
